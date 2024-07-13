8ef29f683f74fc1b1e84820621b19229ad5712fd
Msoaressdp <mssdp@cesar.school>
2024-07-13 19:25:22 -0300

    chore: setup jest with react testing library

---
+++ src/pages/Schedule.spec.jsx
@@ -0,0 +1,7 @@
+import { render, screen } from '@testing-library/react'
+import Schedule from './pages/Schedule';
+describe('Jest', ()  => {
+    it('shoud work', () => {
+        expect(1).toBe(1)
+    })
+})
