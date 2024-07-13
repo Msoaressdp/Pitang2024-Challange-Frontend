163b0b1482e4c14be6a9599b96656cf8cc3bc8a8
Msoaressdp <mssdp@cesar.school>
2024-07-13 18:15:37 -0300

    feat: add chakra dependencies to Schedule

--- src/pages/Schedule.jsx
+++ src/pages/Schedule.jsx
@@ -1,5 +1,20 @@
+import { 
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Box,
+  Heading,
+  VStack,
+  Text,
+ } from '@chakra-ui/react';
 const Schedule = () => (
-    <h1> Schedule </h1>
+
+    <div>
+      <Button colorScheme='facebook' size='lg'>
+        Test
+      </Button>
+    </div>
   );
   
   export default Schedule;
