50cd9fc54a88019653b364fe865d9cb48df69aa5
Msoaressdp <mssdp@cesar.school>
2024-07-13 18:29:14 -0300

    feat: add basic form to Schedule

--- src/pages/Schedule.jsx
+++ src/pages/Schedule.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import { 
   Button,
   FormControl,
@@ -8,13 +9,44 @@ import {
   VStack,
   Text,
  } from '@chakra-ui/react';
-const Schedule = () => (
 
-    <div>
-      <Button colorScheme='facebook' size='lg'>
-        Test
-      </Button>
-    </div>
+const Schedule = () => {
+  const [name, setName] = useState('');
+  const [submittedData, setSubmittedData] = useState(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newAppointment = { name };
+    setSubmittedData(newAppointment);
+    setName('');
+  };
+
+  return (
+    <Box maxW="720px" mx="auto" mt={40} p={6} borderWidth={1} borderRadius="lg" boxShadow="lg">
+      <Heading mb={8} mt={10}>Agendamento</Heading>
+      <form onSubmit={handleSubmit}>
+        <VStack spacing={4} align="stretch">
+          <FormControl>
+            <FormLabel htmlFor="name">Nome:</FormLabel>
+            <Input 
+              id="name" 
+              value={name} 
+              onChange={(e) => setName(e.target.value)} 
+            />
+          </FormControl>
+
+          <Button mt={4} colorScheme="teal" type="submit">Submit</Button>
+        </VStack>
+      </form>
+
+      {submittedData && (
+        <Box mt={4} p={4} borderWidth="1px" borderRadius="lg">
+          <Heading as="h2" size="md">Dados Submetidos</Heading>
+          <Text><strong>Nome:</strong> {submittedData.name}</Text>
+        </Box>
+      )}
+    </Box>
   );
-  
-  export default Schedule;
+};
+
+export default Schedule;
