20998d88f8d0b3b7a32ee4a3a631a24b01a2aa30
Msoaressdp <mssdp@cesar.school>
2024-07-13 18:41:15 -0300

    feat: add scheduled date to form

--- src/pages/Schedule.jsx
+++ src/pages/Schedule.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 import { 
   Button,
   FormControl,
@@ -12,13 +14,15 @@ import {
 
 const Schedule = () => {
   const [name, setName] = useState('');
+  const [scheduledDate, setScheduledDate] = useState(null);
   const [submittedData, setSubmittedData] = useState(null);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    const newAppointment = { name };
+    const newAppointment = { name, scheduledDate };
     setSubmittedData(newAppointment);
     setName('');
+    setScheduledDate(null);
   };
 
   return (
@@ -35,14 +39,32 @@ const Schedule = () => {
             />
           </FormControl>
 
+          <FormControl>
+            <FormLabel htmlFor="appointmentDate">Data e Hora do Agendamento:</FormLabel>
+            <DatePicker
+              id="appointmentDate"
+              selected={scheduledDate}
+              onChange={(date) => setScheduledDate(date)}
+              showTimeSelect
+              timeFormat="HH:mm"
+              timeIntervals={60}
+              timeCaption="Hora"
+              dateFormat="dd/MM/yyyy HH:mm"
+              minDate={new Date()}
+              minTime={new Date(new Date().setHours(11, 0, 0, 0))}
+              maxTime={new Date(new Date().setHours(20, 0))}
+            />
+          </FormControl>
+
           <Button mt={4} colorScheme="teal" type="submit">Submit</Button>
         </VStack>
       </form>
 
       {submittedData && (
         <Box mt={4} p={4} borderWidth="1px" borderRadius="lg">
-          <Heading as="h2" size="md">Dados Submetidos</Heading>
+           <Heading as="h2" size="md">Submitted Data</Heading>
           <Text><strong>Nome:</strong> {submittedData.name}</Text>
+          <Text><strong>Data e Hora do Agendamento:</strong> {submittedData.scheduledDate ? submittedData.scheduledDate.toString() : ''}</Text>
         </Box>
       )}
     </Box>
