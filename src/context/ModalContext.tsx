f62699c37c774c1d465e70f98bf00e47e4ef6ec6
Msoaressdp <mssdp@cesar.school>
2024-07-16 16:11:22 -0300

    refactor: convert ModalContext from javascript to typescript

---
+++ src/context/ModalContext.tsx
@@ -0,0 +1,43 @@
+import React, { createContext, useContext, useState, ReactNode } from 'react';
+
+interface ModalContextProps {
+  isOpen: boolean;
+  message: string;
+  showModal: (msg: string) => void;
+  closeModal: () => void;
+}
+
+const ModalContext = createContext<ModalContextProps | undefined>(undefined);
+
+export const useModal = (): ModalContextProps => {
+  const context = useContext(ModalContext);
+  if (!context) {
+    throw new Error('useModal must be used within a ModalProvider');
+  }
+  return context;
+};
+
+interface ModalProviderProps {
+  children: ReactNode;
+}
+
+export const ModalProvider: React.FC<ModalProviderProps> = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [message, setMessage] = useState('');
+
+  const showModal = (msg: string) => {
+    setMessage(msg);
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+    setMessage('');
+  };
+
+  return (
+    <ModalContext.Provider value={{ isOpen, message, showModal, closeModal }}>
+      {children}
+    </ModalContext.Provider>
+  );
+};
