eace0849faff912486b78f37c3bd6bb224492ba0
Msoaressdp <mssdp@cesar.school>
2024-07-16 15:48:14 -0300

    refactor: convert routes from javascript to typescript

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,14 +1,13 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-
 import Schedule from './pages/Schedule';
 import AppointmentList from './pages/AppointmentList';
 
-const AppRoutes = () => (
+const AppRoutes: React.FC = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Navigate to="/schedule" />} />
-        <Route element={<Schedule />} path='/schedule' />
-        <Route element={<AppointmentList />} path='/list' />
+      <Route path="/schedule" element={<Schedule />} />
+      <Route path="/list" element={<AppointmentList />} />
     </Routes>
   </BrowserRouter>
 );
