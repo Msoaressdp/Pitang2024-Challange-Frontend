a985dc760fbf8907728a7244b7d2ee7ccd5e3eda
Msoaressdp <mssdp@cesar.school>
2024-07-17 20:32:20 -0300

    test(AppointmentItem): add tests to edit, delete and checkbox

---
+++ src/tests/mockAppointment.ts
@@ -0,0 +1,8 @@
+export const mockAppointment = {
+    id: '1',
+    name: 'Matheus Soares da Silva Dantas Pereira',
+    birthDate: new Date('19998-12-01'),
+    scheduledDate: new Date('2024-08-21T11:00:00'),
+    situation: 'Done',
+    conclusion: 'Successfully completed',
+  };
