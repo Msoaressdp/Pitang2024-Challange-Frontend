5c3dbcb8a8a10312e804285461f3cf6ff9fa5ffb
Msoaressdp <mssdp@cesar.school>
2024-07-18 19:30:57 -0300

    refactor: change background color

--- src/pages/Schedule.tsx
+++ src/pages/Schedule.tsx
@@ -5,7 +5,6 @@ import SubmissionModal from '../components/ModalComponent';
 import useFormState from '../hooks/useFormState';
 import DatePickerField from '../components/DatePickerField';
 import { storeAppointment } from '../services/api';
-import { TimeIcon } from '@chakra-ui/icons';
 import {
   Button,
   FormControl,
@@ -21,6 +20,7 @@ import {
   HStack,
   Icon,
 } from '@chakra-ui/react';
+import { TimeIcon } from '@chakra-ui/icons';
 
 const Schedule: React.FC = () => {
   const {
@@ -51,94 +51,96 @@ const Schedule: React.FC = () => {
   };
 
   return (
-    <Container maxW="container.lg" py={12}>
-      <Box
-        bg={useColorModeValue('white', 'gray.700')}
-        boxShadow="lg"
-        p={8}
-        borderRadius="lg"
-      >
-        <HStack justify="center" mb={8}>
-          <Heading textAlign="center">Agendamento de Vacinas Covid-19</Heading>
-          <Icon as={TimeIcon} boxSize={8} color="black" />
-        </HStack>
-        <Divider mb={8} />
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <VStack spacing={8} align="stretch">
-            <FormControl isInvalid={!!errors.name}>
-              <FormLabel htmlFor="name">Nome:</FormLabel>
-              <Input
-                id="name"
-                {...register('name')}
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                placeholder="Digite seu nome completo"
-                variant="filled"
-                size="lg"
-                borderRadius="full"
+    <Box bg={useColorModeValue('#f5f5dc', 'gray.800')} minH="100vh" py={12}>
+      <Container maxW="container.lg">
+        <Box
+          bg={useColorModeValue('white', 'gray.700')}
+          boxShadow="lg"
+          p={8}
+          borderRadius="lg"
+        >
+          <HStack justify="center" mb={8}>
+            <Heading textAlign="center">Agendamento de Vacinas Covid-19</Heading>
+            <Icon as={TimeIcon} boxSize={8} color="black" />
+          </HStack>
+          <Divider mb={8} />
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <VStack spacing={8} align="stretch">
+              <FormControl isInvalid={!!errors.name}>
+                <FormLabel htmlFor="name">Nome:</FormLabel>
+                <Input
+                  id="name"
+                  {...register('name')}
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  placeholder="Digite seu nome completo"
+                  variant="filled"
+                  size="lg"
+                  borderRadius="full"
+                />
+                {errors.name && (
+                  <Text color="red.500" mt={2}>
+                    {(errors.name as any)?.message?.toString()}
+                  </Text>
+                )}
+              </FormControl>
+
+              <DatePickerField
+                control={control}
+                name="birthDate"
+                label="Data de Nascimento:"
+                selectedDate={birthDate}
+                setSelectedDate={setBirthDate}
+                dateFormat="dd/MM/yyyy"
+                maxDate={new Date()}
               />
-              {errors.name && (
-                <Text color="red.500" mt={2}>
-                  {(errors.name as any)?.message?.toString()}
-                </Text>
-              )}
-            </FormControl>
 
-            <DatePickerField
-              control={control}
-              name="birthDate"
-              label="Data de Nascimento:"
-              selectedDate={birthDate}
-              setSelectedDate={setBirthDate}
-              dateFormat="dd/MM/yyyy"
-              maxDate={new Date()}
-            />
+              <DatePickerField
+                control={control}
+                name="scheduledDate"
+                label="Data e Hora do Agendamento:"
+                selectedDate={scheduledDate}
+                setSelectedDate={setScheduledDate}
+                showTimeSelect
+                timeFormat="HH:mm"
+                timeIntervals={60}
+                timeCaption="Hora"
+                dateFormat="dd/MM/yyyy HH:mm"
+                minDate={new Date()}
+                minTime={new Date(new Date().setHours(10, 0, 0, 0))}
+                maxTime={new Date(new Date().setHours(22, 0))}
+              />
 
-            <DatePickerField
-              control={control}
-              name="scheduledDate"
-              label="Data e Hora do Agendamento:"
-              selectedDate={scheduledDate}
-              setSelectedDate={setScheduledDate}
-              showTimeSelect
-              timeFormat="HH:mm"
-              timeIntervals={60}
-              timeCaption="Hora"
-              dateFormat="dd/MM/yyyy HH:mm"
-              minDate={new Date()}
-              minTime={new Date(new Date().setHours(10, 0, 0, 0))}
-              maxTime={new Date(new Date().setHours(22, 0))}
-            />
-
-            <HStack spacing={4} mt={4}>
-              <Button
-                colorScheme="teal"
-                type="submit"
-                size="lg"
-                flex="1"
-                borderRadius="full"
-                py={6}
-              >
-                Agendar
-              </Button>
-              <Link to="/list">
+              <HStack spacing={4} mt={4}>
                 <Button
-                  colorScheme="blue"
+                  colorScheme="teal"
+                  type="submit"
                   size="lg"
                   flex="1"
                   borderRadius="full"
                   py={6}
                 >
-                  Ver Agendamentos
+                  Agendar
                 </Button>
-              </Link>
-            </HStack>
-          </VStack>
-        </form>
+                <Link to="/list">
+                  <Button
+                    colorScheme="blue"
+                    size="lg"
+                    flex="1"
+                    borderRadius="full"
+                    py={6}
+                  >
+                    Ver Agendamentos
+                  </Button>
+                </Link>
+              </HStack>
+            </VStack>
+          </form>
 
-        <SubmissionModal />
-      </Box>
-    </Container>
+          <SubmissionModal />
+        </Box>
+      </Container>
+    </Box>
   );
 };
 
