c57092f0baa4e487acfdcc8a46c2ab5da3b133bc
Msoaressdp <mssdp@cesar.school>
2024-07-19 13:56:14 -0300

    refactor: adjust zod schema logic and add types

--- src/schema/appointmentSchema.ts
+++ src/schema/appointmentSchema.ts
@@ -2,6 +2,10 @@ import * as z from 'zod';
 
 export const appointmentSchema = z.object({
   name: z.string().min(1, 'Nome é obrigatório'),
-  birthDate: z.date({ required_error: 'Data de Nascimento é obrigatória' }),
-  scheduledDate: z.date({ required_error: 'Data e Hora do Agendamento são obrigatórias' }),
+  birthDate: z.union([z.date(), z.null()]).refine((val): val is Date => val !== null, {
+    message: 'Data de Nascimento é obrigatória',
+  }),
+  scheduledDate: z.union([z.date(), z.null()]).refine((val): val is Date => val !== null, {
+    message: 'Data e Hora do Agendamento são obrigatórias',
+  }),
 });
